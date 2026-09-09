22d19f99b0276d40095150de1f8f6569340fc969
novchinnikov13-dotcom <novchinnikov13@gmail.com>
2026-09-09 22:55:26 +0300

    Update conftest.py

--- hw2/conftest.py
+++ hw2/conftest.py
@@ -1,8 +1,10 @@
-import pytest
 from datetime import datetime
 
-from ..hw1.init import create_app, db as _db
-from ..hw1.models import Client, Parking, CParking
+import pytest
+
+from ..hw_1.init import create_app
+from ..hw_1.init import db as _db
+from ..hw_1.models import Client, CParking, Parking
 
 
 @pytest.fixture
@@ -14,28 +16,20 @@ def app():
 
     with _app.app_context():
 
-
         _db.drop_all()
         _db.create_all()
         client = Client(
-            id = 1,
-            name = "Test",
-            surname = 'Name',
-            credit_card = 'Visa',
-            car_number = 'ADC5432F'
+            id=1, name="Test", surname="Name", credit_card="Visa", car_number="ADC5432F"
         )
         parking = Parking(
-            id = 1,
-            address = 'Berlin',
-            opened = True,
-            count_places = 12,
-            count_available_places = 5
+            id=1,
+            address="Berlin",
+            opened=True,
+            count_places=12,
+            count_available_places=5,
         )
         client_park = CParking(
-            client_id = 1,
-            parking_id = 1,
-            time_in=datetime.now(),
-            time_out=None
+            client_id=1, parking_id=1, time_in=datetime.now(), time_out=None
         )
         _db.session.add(client)
         _db.session.add(parking)
@@ -48,9 +42,11 @@ def app():
         _db.session.remove()
         _db.drop_all()
 
+
 @pytest.fixture
 def client(app):
     return app.test_client()
+
 
 @pytest.fixture
 def db(app):
