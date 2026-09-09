5e6a01e8e9e309811eaf0f61779da05bef2af860
novchinnikov13-dotcom <novchinnikov13@gmail.com>
2026-09-09 22:02:46 +0300

    Create init.py

---
+++ hw1/init.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///parking.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+
+    from module_29_testing.hw.hw_1.models import Client, Parking, CParking
+    from module_29_testing.hw.hw2.app import init_routes  # импортируем функцию
+
+    with app.app_context():
+        db.create_all()
+
+    init_routes(app)
+
+    return app
