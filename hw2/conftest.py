54a643fb4c52e68bd40c88f8531d1ec2b719d51c
novchinnikov13-dotcom <novchinnikov13@gmail.com>
2026-09-09 23:55:51 +0300

    Update conftest.py

--- hw2/conftest.py
+++ hw2/conftest.py
@@ -1,7 +1,8 @@
 import pytest
 from flask import Flask
 from hw1.init import db
-
+# Импортируем функцию, которая вешает роуты
+from hw1.app import init_routes  
 
 @pytest.fixture
 def app():
@@ -9,13 +10,16 @@ def app():
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+    # 1. Инициализируем базу данных
     db.init_app(app)
+
+    # 2. !!! ГЛАВНОЕ ИЗМЕНЕНИЕ: Вешаем все роуты на это приложение !!!
+    init_routes(app)
 
     with app.app_context():
         db.create_all()
         yield app
         db.drop_all()
-
 
 @pytest.fixture
 def client(app):
